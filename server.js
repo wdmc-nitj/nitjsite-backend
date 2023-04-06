7735d35837fb9cfd12c19bce62434d115dd16410
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-04-06 17:51:13 +0530

    Start listening after database connection

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ mongoose
   })
   .then(() => {
     console.log("connection to database eshtablished");
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
   })
   .catch((err) => {
     console.log(err);
@@ -28,9 +31,7 @@ mongoose
 
 //server listening------------------------------------------------->
 const port = process.env.PORT || 8000;
-const server = app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+
 
 
 
