e4c7f288f568563e627557389a2a043cda82030a
Aditya Gupta <72199742+Adityagupta1625@users.noreply.github.com>
2023-04-06 18:17:42 +0530

    fixed tender issue

--- controllers/tender.js
+++ controllers/tender.js
@@ -3,44 +3,50 @@ const Tender = require("../models/tender");
 
 //----------------------------------------------------------------------->
 exports.addTender = async (req, res) => {
-    
-    const data = new Tender(req.body);
+  const data = new Tender(req.body);
 
-    data.save()
-        .then(() => res.status(200).send(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+  data
+    .save()
+    .then(() => res.status(200).send(data))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 exports.showTender = async (req, res) => {
+  if (req.params.id !== undefined) {
+    Tender.findById(req.params.id)
+      .then((data) => res.status(200).json(data))
+      .catch((err) => res.status(400).json("Error: " + err));
+  } else {
     Tender.find({ show: true })
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+      .then((data) => res.status(200).json(data))
+      .catch((err) => res.status(400).json("Error: " + err));
+  }
 };
 
 exports.showTenderbyId = async (req, res) => {
-    Tender.findById(req.params.id)
-        .then((data) => res.status(200).json(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+  Tender.findById(req.params.id)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 exports.updateTender = async (req, res) => {
-    Tender.findByIdAndUpdate(req.params.id, req.body)
-        .then(() => {
-            res.status(200).send("Tender updated successfully");
-        })
-        .catch((err) => res.status(400).json("Error: " + err));
+  Tender.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.status(200).send("Tender updated successfully");
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 exports.deleteTender = async (req, res) => {
-    Tender.findByIdAndUpdate(req.params.id, { $set: { show: false } })
-        .then(() => {
-            res.status(200).send("Tender deleted successfully");
-        })
-        .catch((err) => res.status(400).json("Error: " + err));
+  Tender.findByIdAndUpdate(req.params.id, { $set: { show: false } })
+    .then(() => {
+      res.status(200).send("Tender deleted successfully");
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 exports.showAllTender = async (req, res) => {
-    Tender.find({})
-        .then((data) => res.status(200).send(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+  Tender.find({})
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
