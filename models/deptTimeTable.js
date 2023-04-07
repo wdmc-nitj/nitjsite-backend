708787c1bdd35a2954713efabca9e1f355e0fb51
YASH-GU24 <yashg.it.20@nitj.ac.in>
2023-04-07 12:27:41 +0530

    Added models

---
+++ models/deptTimeTable.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+
+const Schema=new mongoose.Schema({
+    department:{
+        type:String,
+        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf']
+    },
+    link: {type:String},
+    type:{
+      type:String,
+      enum:["B.Tech","M.Tech","PhD","PG Diploma"]
+    },
+    
+    sourceOfInfo: {
+        type: Object,
+        default: {
+          name: null,
+          email: null,
+          designation: null,
+          department: null,
+        }
+      },
+    show: { type: Boolean, default: true },
+    order:{
+        type: Number,
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model("deptTimeTable",Schema);
