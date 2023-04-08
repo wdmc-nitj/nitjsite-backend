345105e015e9e35bf37a94f436daf070d8006df1
parteekcoder <parteekgoyal118@gmail.com>
2023-04-08 09:24:53 +0530

    Update Achievement.js

--- models/Achievement.js
+++ models/Achievement.js
@@ -5,8 +5,10 @@ const Schema=new mongoose.Schema({
         type:String,
         enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf']
     },
-    title: {type:String},
-    img: {type:String},
+    achievement:[{
+      title: {type:String},
+    count:{type:String},
+    }],
     is_active:{
       type:Boolean,
       default:true
