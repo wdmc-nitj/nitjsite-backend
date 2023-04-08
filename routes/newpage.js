0e90ffa7f9e3318ac88a1d97004c5e68dd15df86
Prince Verma <princeverma360p@gmail.com>
2023-04-08 21:26:03 +0530

    new page

--- routes/newpage.js
+++ routes/newpage.js
@@ -5,15 +5,13 @@ const newPageController = require("../controllers/newpage");
 //Router
 const Router = express.Router();
 
-Router.route("/").get(newPageController.getNewPage);
+Router.route("/").get(newPageController.getNewPagebyId);
 
-Router.route("/:id").get(newPageController.getNewPagebyId);
-
-// Router.route("/get/all").get(latestNewsController.getAllNews);
+Router.route("/get/all").get(newPageController.getNewPage);
 
 Router.route("/:id")
-    .put(newPageController.updatenewPage)
-    .post(newPageController.deletenewPage);
+  .put(newPageController.updatenewPage)
+  .post(newPageController.deletenewPage);
 
 //Export----------------------------->
 module.exports = Router;
