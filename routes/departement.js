b2f81b6f85769896b04071cff2ed878189bc4daa
Aditya Garg <uber6707@gmail.com>
2023-04-08 21:31:58 +0530

    login error added and updated deptprograminfo

--- routes/departement.js
+++ routes/departement.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const resetController = require('../controllers/resetPassword');
 const latestNewsController = require("../controllers/deptNews");
+const programmeInfoController = require("../controllers/deptProgrammeInfo");
 
 
 const {
@@ -113,60 +114,65 @@ Router.put("/:dept/Faculty/:id", authController.signInAuthentication,updateFacul
 Router.delete("/:dept/Faculty/:id", deleteFaculty);
 
 Router.get("/:dept/Placement", getByDeptPlacement);
-Router.post("/:dept/Placement", addPlacement);
-Router.put("/:dept/Placement/:id", updatePlacement);
-Router.delete("/:dept/Placement/:id", deletePlacement);
+// Router.post("/:dept/Placement", addPlacement);
+// Router.put("/:dept/Placement/:id", updatePlacement);
+// Router.delete("/:dept/Placement/:id", deletePlacement);
+
+Router.get("/:dept/programmeInfo",programmeInfoController.getByDeptprogrammeInfo);
+// Router.post("/:dept/Placement", addPlacement);
+// Router.put("/:dept/Placement/:id", updatePlacement);
+// Router.delete("/:dept/Placement/:id", deletePlacement);
 
 Router.get("/:dept/Infrastructure", getByDeptInfrastructure);
-Router.post("/:dept/Infrastructure", addInfrastructure);
-Router.put("/:dept/Infrastructure/:id", updateInfrastructure);
-Router.delete("/:dept/Infrastructure/:id", deleteInfrastructure);
+// Router.post("/:dept/Infrastructure", addInfrastructure);
+// Router.put("/:dept/Infrastructure/:id", updateInfrastructure);
+// Router.delete("/:dept/Infrastructure/:id", deleteInfrastructure);
 
 Router.get("/:dept/Alumni", getByDeptAlumni);
-Router.post("/:dept/Alumni", addAlumni);
-Router.put("/:dept/Alumni/:id", updateAlumni);
-Router.delete("/:dept/Alumni/:id", deleteAlumni);
+// Router.post("/:dept/Alumni", addAlumni);
+// Router.put("/:dept/Alumni/:id", updateAlumni);
+// Router.delete("/:dept/Alumni/:id", deleteAlumni);
 
 Router.get("/:dept/PhdScholar", getByDeptPhdScholar);
 Router.get("/:dept/PhdScholar/:id",getByIdPhdScholar);
-Router.post("/:dept/PhdScholar", addPhdScholar);
-Router.put("/:dept/PhdScholar/:id", updatePhdScholar);
-Router.delete("/:dept/PhdScholar/:id", deletePhdScholar);
+// Router.post("/:dept/PhdScholar", addPhdScholar);
+// Router.put("/:dept/PhdScholar/:id", updatePhdScholar);
+// Router.delete("/:dept/PhdScholar/:id", deletePhdScholar);
 
 Router.get("/:dept/Staff", getByDeptStaff);
-Router.post("/:dept/Staff", addStaff);
-Router.put("/:dept/Staff/:id", updateStaff);
-Router.delete("/:dept/Staff/:id", deleteStaff);
+// Router.post("/:dept/Staff", addStaff);
+// Router.put("/:dept/Staff/:id", updateStaff);
+// Router.delete("/:dept/Staff/:id", deleteStaff);
 
 Router.get("/:dept/Student", getByDeptStudent);
-Router.post("/:dept/Student", addStudent);
-Router.put("/:dept/Student/:id", updateStudent);
-Router.delete("/:dept/Student/:id", deleteStudent);
+// Router.post("/:dept/Student", addStudent);
+// Router.put("/:dept/Student/:id", updateStudent);
+// Router.delete("/:dept/Student/:id", deleteStudent);
 
 Router.get("/:dept/Activity", getByDeptActivity);
-Router.post("/:dept/Activity", addActivity);
-Router.put("/:dept/Activity/:id", updateActivity);
-Router.delete("/:dept/Activity/:id", deleteActivity);
+// Router.post("/:dept/Activity", addActivity);
+// Router.put("/:dept/Activity/:id", updateActivity);
+// Router.delete("/:dept/Activity/:id", deleteActivity);
 
 Router.get("/:dept/Achievement", getByDeptAchievement);
-Router.post("/:dept/Achievement", addAchievement);
-Router.put("/:dept/Achievement/:id", updateAchievement);
-Router.delete("/:dept/Achievement/:id", deleteAchievement);
+// Router.post("/:dept/Achievement", addAchievement);
+// Router.put("/:dept/Achievement/:id", updateAchievement);
+// Router.delete("/:dept/Achievement/:id", deleteAchievement);
 
 Router.get("/:dept/messageofHOD", getByDeptHOD);
-Router.post("/:dept/messageofHOD", addHOD);
-Router.put("/:dept/messageofHOD/:id", updateHOD);
-Router.delete("/:dept/messageofHOD/:id", deleteHOD);
+// Router.post("/:dept/messageofHOD", addHOD);
+// Router.put("/:dept/messageofHOD/:id", updateHOD);
+// Router.delete("/:dept/messageofHOD/:id", deleteHOD);
 
 Router.get("/:dept/awardsAndHonors", getByDeptAwards);
-Router.post("/:dept/awardsAndHonors", addAwards);
-Router.put("/:dept/awardsAndHonors/:id", updateAwards);
-Router.delete("/:dept/awardsAndHonors/:id", deleteAwards);
+// Router.post("/:dept/awardsAndHonors", addAwards);
+// Router.put("/:dept/awardsAndHonors/:id", updateAwards);
+// Router.delete("/:dept/awardsAndHonors/:id", deleteAwards);
 
 Router.get("/:dept/programmes", getAllProgrammes);
-Router.post("/:dept/programmes", addProgrammes);
-Router.put("/:dept/programmes/:id", updateProgrammes);
-Router.delete("/:dept/programmes/:id", deleteProgrammes);
+// Router.post("/:dept/programmes", addProgrammes);
+// Router.put("/:dept/programmes/:id", updateProgrammes);
+// Router.delete("/:dept/programmes/:id", deleteProgrammes);
 
 Router.post("/:dept/confirmation", resetController.resetEmailHandler);
 Router.get("/:dept/confirmation/:token", resetController.checkToken);
@@ -181,7 +187,6 @@ Router.get("/:dept/Projects",getByDeptProject);
 Router.get("/:dept/SocietyClubs",getByDeptClubs);
 Router.get("/:dept/Images",getByDeptDeptImages);
 Router.get("/:dept/contactus",getByDeptContactUs);
-
 
 Router.route("/:dept/news").get(latestNewsController.getNews);
 Router.route("/:dept/all").get(latestNewsController.getAllNews);
