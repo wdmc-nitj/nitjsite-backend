78762248c418a8e1e64d05787f0161cb286b66bd
Prince Verma <princeverma360p@gmail.com>
2023-04-09 23:08:04 +0530

    file upload done

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,27 +2,15 @@ const fs = require("fs");
 const path = require("path");
 
 exports.upload = async (req, res) => {
+  console.log("in upload function");
+  const file = req.file;
+  console.log(file);
 
-    const file = req.body;
-    console.log(file);
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    res.send(error);
+  }
 
-    if (!file) {
-        const error = new Error("Please upload a file");
-        error.httpStatusCode = 400;
-        res.send(error);
-    }
-
-    fs.writeFile(
-        path.join(__dirname, "../images", file.originalname),
-        file.buffer,
-        (err) => {
-            if (err) {
-                console.log(err);
-            }
-        }
-    );
-    
-    res.json(path.join(__dirname, "../images", file.originalname));
-  };
-  
-
+  res.send(path.join(__dirname, "../files", file.originalname));
+};
