0a7ce1b482e6a43169f8203aca27c98f74baab6d
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2023-04-11 10:38:11 +0530

    Revert "Login functionality"

--- models/deptProgrammeInfo.js
+++ models/deptProgrammeInfo.js
@@ -3,29 +3,23 @@ const mongoose=require('mongoose');
 const Schema=new mongoose.Schema({
     department:{
         type:String,
-        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf','cee','cai']
+        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf']
     },
-    "Program of Study":{
-      type:String
+    description: {type:String},
+    type:{
+      type:String,
+      enum:["Curriculum","Program Outcomes","Program Educational Objectives"]
     },
-    branch:{
-      type:String
-    },
-    "Program Specific Outcomes":{
-      type:[String]
-    },
-    "Program Outcomes":{
-      type:[String]
-    },
-    "Program Educational Objectives":{
-      type:[String]
-    },
-    show:{
-      type:Boolean,
-    },
+    
     sourceOfInfo: {
-      type:  String
-  },
+        type: Object,
+        default: {
+          name: null,
+          email: null,
+          designation: null,
+          department: null,
+        }
+      }
 },{timestamps:true})
 
 module.exports=mongoose.model("deptProgrammeInfo",Schema);
--- models/deptTimeTable.js
+++ models/deptTimeTable.js
@@ -3,16 +3,23 @@ const mongoose=require('mongoose');
 const Schema=new mongoose.Schema({
     department:{
         type:String,
-        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf','cee','cai']
+        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf']
     },
     link: {type:String},
     type:{
       type:String,
       enum:["B.Tech","M.Tech","PhD","PG Diploma"]
     },
+    
     sourceOfInfo: {
-      type:  String
-  },
+        type: Object,
+        default: {
+          name: null,
+          email: null,
+          designation: null,
+          department: null,
+        }
+      }
 },{timestamps:true})
 
 module.exports=mongoose.model("deptTimeTable",Schema);
